a4785a3eaa13da854c86c14dafc9817469ec66c7
sdsouza33 <dsouzasunnny133@gmail.com>
2021-11-01 10:11:53 +0530

    fixed bugs 2

--- Calc.js
+++ Calc.js
@@ -2,7 +2,14 @@ total = ''
 
 function input_numbers(numbers){
 	
+if(numbers == 'del'){
+
+total = total.slice(0, -1);
+document.getElementById("screen").value = total
+
+}else{
     val = document.getElementById(numbers).value
     total += val
     document.getElementById("screen").value = total
+       }
 }
